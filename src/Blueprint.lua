f302f2922169818df99a7fa26f18da48ad63f144
Ossie <45662054+OssieNomae@users.noreply.github.com>
2024-04-15 17:01:43 +0300

    1.1.0 - Team create patch
    - Fixed team create live scripting from blocking the source from being changed due to being "read-only" at start up for a short time
    - Removed Debug
    - Minor restructuring

--- src/Blueprint.lua
+++ src/Blueprint.lua
@@ -1,6 +1,6 @@
 --[[
 	License: Licensed under the MIT License
-	Version: 1.0.2
+	Version: 1.1.0
 	Github: https://github.com/OssieNomae/Blueprint
 	Authors:
 		OssieNomae - 2024
@@ -21,11 +21,22 @@ local ScriptEditorService = game:GetService("ScriptEditorService")
 ----- Private Variables -----
 local Toolbar = plugin:CreateToolbar("Blueprint") :: PluginToolbar
 local SetBlueprint = Toolbar:CreateButton("Blueprint Set", "Select a Script and Press to Set as a Blueprint", "rbxassetid://16867772594", "Set") :: PluginToolbarButton
-local DebugBlueprint = Toolbar:CreateButton("Blueprint Debug", "Toggle Debug for Blueprint", "rbxassetid://16865274680", "Debug") :: PluginToolbarButton
-
-local Blueprint_Debug: boolean = false
 
 ----- Private Methods -----
+local function IsNewScript(Script: Script, Source: string): boolean
+	if Script:IsA("LocalScript") or Script:IsA("Script") then
+		if Source == 'print("Hello world!")\n' then
+			return true
+		end
+	elseif Script:IsA("ModuleScript") then
+		if Source == 'local module = {}\n\nreturn module\n' then 
+			return true
+		end
+	end
+
+	return false
+end
+
 local function ReplaceBlueprintSource()
 	local Selected = Selection:Get()
 	if #Selected < 1 then
@@ -46,22 +57,7 @@ local function ReplaceBlueprintSource()
 	print(`Blueprint: Successfully set {Script.ClassName} blueprint source as ->`, {ScriptSource})
 end
 
-local function IsNewScript(Script: Script, Source: string): boolean
-	if Script:IsA("LocalScript") or Script:IsA("Script") then
-		if Source == 'print("Hello world!")\n' then
-			return true
-		end
-	elseif Script:IsA("ModuleScript") then
-		if Source == 'local module = {}\n\nreturn module\n' then 
-			return true
-		end
-	end
-	
-	return false
-end
-	
------ Connections -----
-ScriptEditorService.TextDocumentDidOpen:Connect(function(ScriptDocument)
+local function ReplaceSource(ScriptDocument)
 	local Script = ScriptDocument:GetScript()
 	if not Script then return end
 
@@ -69,22 +65,50 @@ ScriptEditorService.TextDocumentDidOpen:Connect(function(ScriptDocument)
 
 	local BlueprintSource = plugin:GetSetting(`{Script.ClassName}-BlueprintSource`)
 	if not BlueprintSource then return end
-
+	
+	-- Source replace verification for Team Create
+	local ChangedConnection
+	local CloseConnection
+	
+	local function Disconnect()
+		ChangedConnection:Disconnect()
+		CloseConnection:Disconnect()
+	end
+	
+	ChangedConnection = ScriptEditorService.TextDocumentDidChange:Connect(function(ChangedScript, Changes)
+		if ChangedScript ~= ScriptDocument then
+			return
+		end
+		
+		local Script = ScriptDocument:GetScript()
+		if not Script then return end
+		
+		Disconnect()
+		
+		if Script.Source ~= BlueprintSource then
+			ReplaceSource(ScriptDocument) -- Recursive
+		end
+	end)
+	
+	CloseConnection = ScriptEditorService.TextDocumentDidClose:Connect(function(ClosedScript)
+		if ClosedScript ~= ScriptDocument then
+			return
+		end
+		
+		Disconnect()
+	end)
+	
+	-- Actually replace the script
 	ScriptEditorService:UpdateSourceAsync(Script, function()
 		return BlueprintSource
 	end)
-
-	if Blueprint_Debug then
-		print(`Blueprint Debug: Successfully replaced {Script:GetFullName()} with a {Script.ClassName} blueprint`)
-	end
+end
+	
+----- Connections -----
+ScriptEditorService.TextDocumentDidOpen:Connect(function(ScriptDocument)
+	ReplaceSource(ScriptDocument)
 end)
 
 SetBlueprint.Click:Connect(function()
 	ReplaceBlueprintSource()
 end)
-
-DebugBlueprint.Click:Connect(function()
-	Blueprint_Debug = not Blueprint_Debug
-	DebugBlueprint:SetActive(Blueprint_Debug)
-end)
-DebugBlueprint:SetActive(Blueprint_Debug)
